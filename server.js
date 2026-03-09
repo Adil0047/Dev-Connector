fa2d4c583552d64cedc1a3276fa2b21e7af20cfc
M.Adil Naeem <madilnaeem047@gmail.com>
2026-03-09 11:23:39 +0500

    second commit

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/',(req, res) => {
-      res.send(`api running :)`)
+//connect Database
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send(`api running :)`);
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-      console.log(`server running at port: ${PORT}`);
-})
+  console.log(`server running at port: ${PORT}`);
+});
