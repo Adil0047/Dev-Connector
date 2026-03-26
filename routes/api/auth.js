808972fc5e2af2ab7f7a56c1ec08cbf06e8ebaf4
M.Adil Naeem <madilnaeem047@gmail.com>
2026-03-26 10:08:15 +0500

    eighth commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 //@Route          api/auth
 //@Desc            Test route
-//@Access         Public
+//@Access         Private
 
-router.get('/', (req, res) => {
-  res.send('Boom! Auth Route :)');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    return res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+  // res.send('Boom! Auth Route :)');
 });
 
 module.exports = router;
